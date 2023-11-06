82cc3d911d171d0fa300599d461758c4863b13ac
Tjommiboy <119463874+Tjommiboy@users.noreply.github.com>
2023-11-07 01:20:54 +0700

    Tues 7 nov

---
+++ scripts/getsynths.js
@@ -0,0 +1,47 @@
+const api_url = 'https://www.fjord1design.com/pe1/wp-json/wp/v2/posts?_embed';
+const boardsElement = document.querySelector('.boardscontainer');
+
+export async function getSynths() {
+
+
+  try {
+    
+    const response = await fetch(api_url);
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+   
+    const data = await response.json();
+    console.log(data);
+
+    for (let i = 0; i < data.length && i < 8; i++) {
+      const post = data[i];
+
+    
+      const productElement = document.createElement('div');
+      productElement.classList.add('boardsposts');
+      boardsElement.appendChild(productElement);
+
+      const titleElement = document.createElement('h2');
+      titleElement.classList.add('boardcard')
+      titleElement.textContent = post.title.rendered;
+      productElement.appendChild(titleElement);
+
+      if (post.featured_media) {
+       
+        const imageUrl = post._embedded['wp:featuredmedia']['0'].source_url;
+        const imageElement = document.createElement('img');
+        imageElement.classList.add('imgboards')
+        imageElement.src = imageUrl;
+        productElement.appendChild(imageElement);
+        console.log(post.featured_media);
+      }
+    }
+  } catch (error) {
+    console.error(error);
+    boardsElement.innerHTML = 'An error occurred while fetching data.';
+  }
+}
+
+
